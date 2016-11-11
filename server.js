b62d66b170b8a304519dfd6c91bbf5c6f037e292
Almouro <contact@almouro.com>
2016-11-11 18:56:00 +0100

    Add a route displaying connecting Android devices

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const installer = require('./installer');
+const androidClient = require('./android-client');
 
 const app = express();
 
 app.get('/install/:hockeyAppId', (req, res) =>
   installer.getMyApp(req.params.hockeyAppId)
   .then(() => res.send('Done'))
+);
+
+app.get('/devices', (req, res) =>
+  androidClient.getDevices()
+  .then(devices => res.json(devices))
 );
 
 app.listen(3000, () => {
